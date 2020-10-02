7f9bd9a6671753effe59dcbc135b555634f8ac31
vitdevelop <balanici_vitalik@mail.ru>
2020-10-02 17:32:41 +0300

    Add macbook keyboard/display config

--- install/macbook/install-macbook-dependencies.sh
+++ install/macbook/install-macbook-dependencies.sh
@@ -13,6 +13,7 @@ packages=(
     xf86-video-fbdev
     xf86-input-synaptics
     powertop
+    tlp
 )
 
 func_iterate_install "${packages[@]}"
@@ -22,6 +23,7 @@ packages=(
 kbdlight
 light-git
 bcwc-pcie-git
+macbook-lighter
 )
 func_iterate_install_aur "${packages[@]}"
 
@@ -29,6 +31,7 @@ systemctl enable --now NetworkManager
 
 sudo cp -f ./powertop.service /etc/systemd/system/powertop.service
 systemctl enable --now powertop.service
+systemctl enable --now macbook-lighter.service
 
 ###############################################################################
 
