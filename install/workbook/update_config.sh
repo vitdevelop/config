f91a1dc57856476fe957b28b942136a1389ef50b
vitdevelop <balanici_vitalik@mail.ru>
2022-11-02 12:23:21 +0200

    Add workbook config

---
+++ install/workbook/update_config.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+rm -rf $HOME/.config/polybar
+ln -sf $HOME/.config/install/workbook/polybar $HOME/.config/polybar
+
+ln -sf $HOME/.config/install/workbook/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+
