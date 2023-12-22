ddf4bfe8dc69a51dc2c6c7cec4d9396faeec1097
vitdevelop <balanici_vitalik@mail.ru>
2023-12-22 12:14:02 +0200

    Fix betterlockscreen layout

--- bin/dmenu-poweroff.sh
+++ bin/dmenu-poweroff.sh
@@ -4,12 +4,13 @@ choices=" Lock\n Logout\n Suspend\n󰒲 Hibernate\n Reboot\n Powe
 font="Symbols Nerd Font:Regular:size=14"
 
 chosen=$(echo -e "$choices" | dmenu -fn "$font" -p ' Computer' -i)
+lockscreen_command="betterlockscreen -l dimblur --show-layout"
 
 case "$chosen" in
-    *Lock) betterlockscreen -l dimblur ;;
+    *Lock) $lockscreen_command ;;
     *Logout) bspc quit ;;
-    *Suspend) systemctl suspend; betterlockscreen -l dimblur ;;
-    *Hibernate) systemctl hibernate; betterlockscreen -l dimblur ;;
+    *Suspend) systemctl suspend; $lockscreen_command ;;
+    *Hibernate) systemctl hibernate; $lockscreen_command ;;
     *Reboot) reboot ;;
     *Poweroff) poweroff ;;
 esac
