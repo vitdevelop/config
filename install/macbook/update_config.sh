9a6511f055807b71a6d12f45f22a042e7995a3ea
vitdevelop <balanici_vitalik@mail.ru>
2026-02-18 02:13:21 +0200

    Add macbook waybar, river configuration

--- install/macbook/update_config.sh
+++ install/macbook/update_config.sh
@@ -6,5 +6,7 @@ ln -sf $HOME/.config/install/macbook/polybar $HOME/.config/polybar
 ln -sf $HOME/.config/install/macbook/xprofile $HOME/.xprofile
 ln -sf $HOME/.config/install/macbook/xresources $HOME/.Xresources
 ln -sf $HOME/.config/install/macbook/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+ln -sf $HOME/.config/install/macbook/waybar/config.jsonc $HOME/.config/waybar/config.jsonc
+ln -sf $HOME/.config/install/macbook/river/init $HOME/.config/river/init
 
 
